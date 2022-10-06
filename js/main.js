6af704123da922d7cd48e739511a59ba02d4925e
AlexanderPonarin <114759942+AlexanderPonarin@users.noreply.github.com>
2022-10-06 20:53:40 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,31 +1,29 @@
-//Функция, возвращающая случайное целое число из переданного диапазона включительно.
 
-let RandomInteger  = function getRandomInteger(min, max){
-   if(typeof(min) != 'number' && typeof(max) != 'number'){   //check type argument
-  
-  return console.log(RandomInteger = NaN);
- }
-  if ( min > max && Math.sign(min) >= 0 && Math.sign(max) >= 0 ) { // swap arguments if it need
-        min = min + max;        
-        max = min - max;
-        min = min - max;
-    return Math.floor(Math.random() * (max - min + 1)) ;
-   }
-   else if ( Math.sign(min) >= 0 && Math.sign(max) >= 0 ) {
-    return Math.floor(Math.random() * (max - min + 1)) ;
- }     
-    
-   return console.log(RandomInteger = NaN);   
- }
 
-console.log(RandomInteger(0,10));
+let getRandomInteger = (min, max) =>{
+  if(typeof(min) !== 'number' && typeof(max) !=='number') {   //check type argument
+   return  NaN;
+}
+ if ( min > max && min >= 0 && max >= 0 ) { // swap arguments if it need
+       min = min + max;        
+       max = min - max;
+       min = min - max;
+   return Math.floor(Math.random() * (max - min + 1)) ;
+  }
+  else if ( min >= 0 && max >= 0 ) {
+   return Math.floor(Math.random() * (max - min + 1)) ;
+}     
+   
+  return  NaN;   
+}
+
 
 // проверка максимальной длины строки
 
-let testString = function getMaxString (textString, maxLength) {
-  
-  if(textString.length > maxLength ){
-    return console.log(testString = false);
+let checkStringLength = (textString, maxLength) => {
+
+  if (textString > maxLength) {
+    return false;
   }
-  return console.log(testString = true);
+    return true;
 }
