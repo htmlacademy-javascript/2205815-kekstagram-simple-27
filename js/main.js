74e034255f18eddbd582e7664f6f64b29e1e1b9e
AlexanderPonarin <114759942+AlexanderPonarin@users.noreply.github.com>
2022-10-06 21:44:00 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 
 
-let getRandomInteger = (min, max) =>{
-  if(typeof(min) !== 'number' && typeof(max) !=='number') {   //check type argument
-   return  NaN;
-}
- if ( min > max && min >= 0 && max >= 0 ) { // swap arguments if it need
-       min = min + max;        
-       max = min - max;
-       min = min - max;
+getRandomInteger = (min, max) =>{
+  if(typeof(min) !== 'number' && typeof(max) !== 'number') {
+    return NaN;
+  }
+  if ( min > max && min >= 0 && max >= 0 ) { // swap arguments if it need
+    min = min + max;
+    max = min - max;
+    min = min - max;
    return Math.floor(Math.random() * (max - min + 1)) ;
   }
   else if ( min >= 0 && max >= 0 ) {
@@ -20,7 +20,7 @@ let getRandomInteger = (min, max) =>{
 
 // проверка максимальной длины строки
 
-let checkStringLength = (textString, maxLength) => {
+checkStringLength = (textString, maxLength) => {
 
   if (textString > maxLength) {
     return false;
