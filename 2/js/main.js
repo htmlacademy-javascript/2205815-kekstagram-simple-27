6e985631df9a8ab192e3a54c501406c0ce8c0e5c
Keks <keks@htmlacademy.ru>
2022-10-09 00:24:51 +0300

    :heavy_check_mark: Сборка #2

--- 2/js/main.js
+++ 2/js/main.js
@@ -1,16 +1,13 @@
 
 
 const getRandomInteger = (min, max) =>{
-  if(typeof(min) !== 'number' && typeof(max) !== 'number') {
-    return NaN;
-  }
-  if ( min > max && min >= 0 && max >= 0 ) { // swap arguments if it need
+  if (min > max && min >= 0 && max >= 0) { // swap arguments if it need
     min = min + max;
     max = min - max;
     min = min - max;
     return Math.floor(Math.random() * (max - min + 1)) ;
   }
-  else if ( min >= 0 && max >= 0 ) {
+  else if (min >= 0 && max >= 0) {
     return Math.floor(Math.random() * (max - min + 1)) ;
   }
 
@@ -20,11 +17,7 @@ getRandomInteger(1, 10);
 
 // проверка максимальной длины строки
 
-const checkStringLength = (textString, maxLength) => {
+const checkStringLength = (textString, maxLength) =>
+  (textString.length > maxLength) ? 'false' : 'true';
 
-  if (textString > maxLength) {
-    return false;
-  }
-  return true;
-};
 checkStringLength('work', 10);
