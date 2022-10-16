c3e888df8c784f5c18f3b96a41ccc0ca5ab05cc9
AlexanderPonarin <114759942+AlexanderPonarin@users.noreply.github.com>
2022-10-16 23:20:59 +0300

    module

---
+++ js/util.js
@@ -0,0 +1,17 @@
+const getRandomInteger = (min, max) => {
+    if (min > max && min >= 0 && max >= 0) { // swap arguments if it need
+      min = min + max;
+      max = min - max;
+      min = min - max;
+      return Math.floor(Math.random() * (max - min + 1));
+    } else if (min >= 0 && max >= 0) {
+      return Math.floor(Math.random() * (max - min + 1));
+    }
+  
+    return NaN;
+  };
+
+const checkStringLength = (textString, maxLength) =>
+  (textString.length < maxLength);
+
+export {getRandomInteger};
