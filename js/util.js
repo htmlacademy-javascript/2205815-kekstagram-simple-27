b909f76e28a85f4fb9f6eb4e4af49d52765b256a
AlexanderPonarin <114759942+AlexanderPonarin@users.noreply.github.com>
2022-10-16 23:28:53 +0300

    Update util.js

--- js/util.js
+++ js/util.js
@@ -1,8 +1,8 @@
 const getRandomInteger = (min, max) => {
-    if (min > max && min >= 0 && max >= 0) { // swap arguments if it need
-      min = min + max;
-      max = min - max;
-      min = min - max;
+  if (min > max && min >= 0 && max >= 0) { // swap arguments if it need
+    min = min + max;
+    max = min - max;
+    min = min - max;
       return Math.floor(Math.random() * (max - min + 1));
     } else if (min >= 0 && max >= 0) {
       return Math.floor(Math.random() * (max - min + 1));
