a70ca3ccdb8a84b645eca2f956b2e7263d788a3e
AlexanderPonarin <114759942+AlexanderPonarin@users.noreply.github.com>
2022-10-25 14:04:30 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,30 +1,3 @@
 import {getPhotos} from './data.js';
 import './photoRendering.js';
 getPhotos(25);
-
-
-
-/*
-//-----------------------------------------------------//
-import {createPhoto} from './data.js';
-
-let pictureList = document.querySelector('.pictures')
-// нашли шаблон
-let templateList = document.querySelector('#picture').content.querySelector('.picture')
-// показали фото других пользователей
-let hiddenTitle = document.querySelector('.pictures__title').classList.remove('visually-hidden')
-
-
-
-let anothePhotos = getPhotos(25)
-console.log(anothePhotos)
-
-anothePhotos.forEach((photo) => {
-  let photoElement = templateList.cloneNode(true)
-  photoElement.querySelector('.picture__img').src = photo.url
-  photoElement.querySelector('.picture__img').alt = photo.description
-  photoElement.querySelector('.picture__likes').textContent = photo.likes
-  photoElement.querySelector('.picture__comments').textContent  = photo.comments
-  pictureList.appendChild(photoElement)
-  
-});*/
