9882811a029f074aad4aaa555315e229a589db5e
AlexanderPonarin <114759942+AlexanderPonarin@users.noreply.github.com>
2022-11-15 00:17:42 +0300

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,31 +1,31 @@
 export const getDataPictures = (onSuccess) => {
-    fetch('https://27.javascript.pages.academy/kekstagram-simple/data')
-      .then((response) => {
-        if (response.ok) {
-          return response.json();
+  fetch('https://27.javascript.pages.academy/kekstagram-simple/data')
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
         };
         return Promise.reject(`${response.status} ${response.statusText}`);
       })
-      .then((res) => {
+    .then((res) => {
         onSuccess(res);
       })
-      .catch ((error) => alert(`Ошибка ${error}, попробуйте еще раз`));
+    .catch ((error) => alert(`Ошибка ${error}, попробуйте еще раз`));
   };
   
-  export const postDataPictures = (body, onSuccess, onError) => {
-    fetch('https://27.javascript.pages.academy/kekstagram-simple',
-      {
-        method: 'POST',
-        body,
-      })
-      .then((response) => {
-        if (response.ok){
-          onSuccess();
-          return;
-        };
+export const postDataPictures = (body, onSuccess, onError) => {
+  fetch('https://27.javascript.pages.academy/kekstagram-simple',
+    {
+      method: 'POST',
+      body,
+    })
+    .then((response) => {
+      if (response.ok){
+        onSuccess();
+        return;
+      };
         return Promise.reject(`${response.status} ${response.statusText}`);
       })
-      .catch((error) => {
-        onError(error);
-      });
-  };
+    .catch((error) => {
+      onError(error);
+  });
+};
