a2e0ca04f0892284c99f32846c980baf6411af3c
Keks <keks@htmlacademy.ru>
2022-11-22 19:39:05 +0300

    :heavy_check_mark: Сборка #8

--- 8/js/form.js
+++ 8/js/form.js
@@ -1,27 +1,69 @@
-const uploadBtn = document.querySelector('.img-upload__input');
-const imageFormEdit = document.querySelector('.img-upload__overlay');
-const closeBtn = imageFormEdit.querySelector('.img-upload__cancel');
-const imageForm = document.querySelector('#upload-select-image');
+import {postDataPictures} from './api.js';
+import {showSuccessDialog, showErrorDialog} from './dialogs.js';
+import {setInitialScale} from './picture-scale.js';
 
-const closeFormClickHandler = () => {
+const uploadButton = document.querySelector('.img-upload__input');
+const imageFormEdit = document.querySelector('.img-upload__overlay');
+const closeButton = imageFormEdit.querySelector('.img-upload__cancel');
+const imageForm = document.querySelector('#upload-select-image');
+const submitButton = imageForm.querySelector('#upload-submit');
+const imagePreview = document.querySelector('.img-upload__preview');
+const documentBody = document.querySelector('body');
+
+const resetImageEffects = () => {
+  imagePreview.classList = '';
+  imagePreview.removeAttribute('style');
+};
+
+const disableSubmitButton = () => {
+  submitButton.textContent = 'Опубликовать';
+  submitButton.disabled = true;
+};
+
+export const enableSubmitButton = () => {
+  submitButton.textContent = 'Опубликовать';
+  submitButton.disabled = false;
+};
+
+export const imageFormEditClickHandler = () => {
   imageFormEdit.classList.add('hidden');
+  documentBody.classList.remove('modal-open');
   imageForm.reset();
+  setInitialScale();
+  resetImageEffects();
+};
+
+const imageFormEditKeydownHandler = (evt) => {
+  if (evt.key === 'Escape') {
+    imageFormEditClickHandler();
+  }
+};
+
+const onSuccess = () => {
+  showSuccessDialog();
+  enableSubmitButton();
+  imageFormEditClickHandler();
+};
+
+const onError = () => {
+  showErrorDialog();
+  enableSubmitButton();
 };
 
 const formSubmitHandler = (evt) => {
   evt.preventDefault();
-  closeFormClickHandler();
+  disableSubmitButton();
+  const formData = new FormData(evt.target);
+  postDataPictures(formData, onSuccess, onError);
 };
 
-const cancelKeydownHandler = (evt) => {
-  if(evt.key === 'Escape'){
-    closeFormClickHandler();
-  }
-};
-
-uploadBtn.addEventListener('change', () => {
-  document.addEventListener('keydown', cancelKeydownHandler);
+const uploadChangeButtonHandler = () => {
+  imageForm.addEventListener('keydown', imageFormEditKeydownHandler);
   imageForm.addEventListener('submit', formSubmitHandler);
-  closeBtn.addEventListener('click', closeFormClickHandler);
+  closeButton.addEventListener('click', imageFormEditClickHandler);
   imageFormEdit.classList.remove('hidden');
-});
+  documentBody.classList.add('modal-open');
+};
+
+uploadButton.addEventListener('change', uploadChangeButtonHandler);
+
