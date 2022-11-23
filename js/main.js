dc7af3b602148fb9ade4f38a07decd23b0ef5ffd
AlexanderPonarin <114759942+AlexanderPonarin@users.noreply.github.com>
2022-11-23 14:56:11 +0300

    22

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
 import {renderPhotos} from './users-pictures.js';
 import './form.js';
+import './picture-effects.js';
+import './picture-scale.js';
+import {getDataPictures} from './api.js';
+import {showDataErrorMessage} from './dialogs.js';
 
-const onSuccess = (photos) => {
-  renderPhotos(photos);
+const onSuccess = (data) => {
+  renderPhotos(data);
 };
 
 const onError = () => {
